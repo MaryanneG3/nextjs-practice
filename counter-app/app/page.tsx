74adf3eac6aa620a8114dbd9332875e827e9a607
Maryanne G <maryanneg@missionreadyhq.com>
2025-02-13 11:24:32 +1300

    added customizable conter component to page.tsx

--- counter-app/app/page.tsx
+++ counter-app/app/page.tsx
@@ -1,13 +1,14 @@
 import Counter from "./components/counter";
+import CustomizableCounter from "./components/customizableCounter";
 import JotaiCounter from "./components/jotaiCounter";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="grid grid-cols-2 gap-10 p-4">
-        {" "}
+      <div className="grid grid-cols-1 sm:grid-cols-2  gap-10 p-4">
         <Counter />
         <JotaiCounter />
+        <CustomizableCounter />
       </div>
     </div>
   );
